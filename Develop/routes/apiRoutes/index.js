8b67d70790977e5605861f95230c1b828da1d045
JHallUofA <83625694+JHallUofA@users.noreply.github.com>
2021-08-29 18:16:32 -0700

    finished routing

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -1,8 +1,29 @@
 const router = require('express').Router();
-const [ db ] = require('../../db/db.json');
+const fs = require('fs');
+const path = require('path');
+const db = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
-    res.json([db]);
+    res.json(db);
 });
+
+router.post('/notes', (req, res) => {
+    db.push(req.body);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(db, null, 2)
+    );
+    res.json(db);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    db.splice(req.params.id - 1, 1)
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db.json'),
+        JSON.stringify(db, null, 2)
+    );
+    res.json(db);
+});
+
 
 module.exports = router
--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const bp = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 app.use('/api', require('./routes/apiRoutes'));
 app.use('/', require('./routes/htmlRoutes'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
